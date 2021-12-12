3b4bb8862b0b0a138565c2e8ee24a724904f6931
Erik Dubois <erik.dubois@gmail.com>
2021-12-12 12:34:42 +0100

    calamares 49.1

--- installation-scripts/40-build-the-iso-local-again.sh
+++ installation-scripts/40-build-the-iso-local-again.sh
@@ -29,7 +29,7 @@ echo
 	desktop="cutefish"
 	dmDesktop="cutefish-xsession"
 
-	arcolinuxVersion='v22.01.06'
+	arcolinuxVersion='v22.01.07'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
