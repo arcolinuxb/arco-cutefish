3d80f86f02b95d6e166a43b98e36a3e30f09f304
arcolinuxz <arcolinuxinfo@gmail.com>
2023-02-02 21:08:04 +0100

    bump nr

--- installation-scripts/30-build-the-iso-the-first-time.sh
+++ installation-scripts/30-build-the-iso-the-first-time.sh
@@ -29,7 +29,7 @@ echo
 	desktop="cutefish"
 	dmDesktop="cutefish-xsession"
 
-	arcolinuxVersion='v23.02.02'
+	arcolinuxVersion='v23.03.01'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
