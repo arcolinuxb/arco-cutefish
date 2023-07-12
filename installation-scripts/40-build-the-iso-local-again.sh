5df5a29fc4db44c117cce8450c78dadac0ddf23b
arcolinuxz <arcolinuxinfo@gmail.com>
2023-07-12 10:28:42 +0200

    new calamares from 23 07 12

--- installation-scripts/40-build-the-iso-local-again.sh
+++ installation-scripts/40-build-the-iso-local-again.sh
@@ -29,7 +29,7 @@ echo
 	desktop="cutefish"
 	dmDesktop="cutefish-xsession"
 
-	arcolinuxVersion='v23.09.01'
+	arcolinuxVersion='v23.09.02'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
