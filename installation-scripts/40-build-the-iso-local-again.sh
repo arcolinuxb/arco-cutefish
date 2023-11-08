cd8e5cf6c112c6424210900e6b350a82b1deaf03
arcolinuxz <arcolinuxinfo@gmail.com>
2023-11-08 22:12:37 +0100

    libadwaita without

--- installation-scripts/40-build-the-iso-local-again.sh
+++ installation-scripts/40-build-the-iso-local-again.sh
@@ -29,7 +29,7 @@ echo
 	desktop="cutefish"
 	dmDesktop="cutefish-xsession"
 
-	arcolinuxVersion='v23.12.01'
+	arcolinuxVersion='v23.12.02'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
