897215982f2a16facd7a0dc254d889dec6e4986f
arcolinuxz <arcolinuxinfo@gmail.com>
2024-03-10 06:35:01 +0100

    kvantum conflict

--- installation-scripts/30-build-the-iso-the-first-time.sh
+++ installation-scripts/30-build-the-iso-the-first-time.sh
@@ -29,7 +29,7 @@ echo
 	desktop="cutefish"
 	dmDesktop="cutefish-xsession"
 
-	arcolinuxVersion='v24.04.01'
+	arcolinuxVersion='v24.04.02'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
